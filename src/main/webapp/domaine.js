4969f0a672f5d16478f63991e417c983dbdcd153
Kurokuni <jacques.burie@isep.fr>
2018-05-29 15:36:32 +0200

    Page d'acceuil et connexion

--- src/main/webapp/domaine.js
+++ src/main/webapp/domaine.js
@@ -5,19 +5,19 @@ $(document).ready(function(){
 		}
 		switch($(this).val()) {
 	    case "info":
-	        $(this).after('<form id="fromMetier"><input type="radio" name="metier" value="ing" checked> Ingénieur Informatique<br><input type="radio" name="metier" value="cho"> Chômageur Informatique<br><input type="radio" name="metier" value="sta"> Stagiaire Informatique<br><input type="submit"></form>');
+	        $(this).after('<form id="fromMetier"><div><input type="radio" name="metier" value="ing" checked> Ingénieur Informatique<br><input type="radio" name="metier" value="cho"> Chômageur Informatique<br><input type="radio" name="metier" value="sta"> Stagiaire Informatique<br></div><div><input type="submit"></div></form>');
 	        break;
 	    case "res":
-	    	$(this).after('<form id="fromMetier"><input type="radio" name="metier" value="ing" checked> Ingénieur Réseaux<br><input type="radio" name="metier" value="cho"> Chômageur Réseaux<br><input type="radio" name="metier" value="sta"> Stagiaire Réseaux<br><input type="submit"></form>');
+	    	$(this).after('<form id="fromMetier"><div><input type="radio" name="metier" value="ing" checked> Ingénieur Réseaux<br><input type="radio" name="metier" value="cho"> Chômageur Réseaux<br><input type="radio" name="metier" value="sta"> Stagiaire Réseaux<br></div><div><input type="submit"></div></form>');
 	        break;
 	    case "sysemb":
-	    	$(this).after('<form id="fromMetier"><input type="radio" name="metier" value="ing" checked> Ingénieur System Embarque<br><input type="radio" name="metier" value="cho"> Chômageur System Embarque<br><input type="radio" name="metier" value="sta"> Stagiaire System Embarque<br><input type="submit"></form>');
+	    	$(this).after('<form id="fromMetier"><div><input type="radio" name="metier" value="ing" checked> Ingénieur System Embarque<br><input type="radio" name="metier" value="cho"> Chômageur System Embarque<br><input type="radio" name="metier" value="sta"> Stagiaire System Embarque<br></div><div><input type="submit"></div></form>');
 	        break;
 	    case "elc":
-	    	$(this).after('<form id="fromMetier"><input type="radio" name="metier" value="ing" checked> Ingénieur Électronique<br><input type="radio" name="metier" value="cho"> Chômageur Électronique<br><input type="radio" name="metier" value="sta"> Stagiaire Électronique<br><input type="submit"></form>');
+	    	$(this).after('<form id="fromMetier"><div><input type="radio" name="metier" value="ing" checked> Ingénieur Électronique<br><input type="radio" name="metier" value="cho"> Chômageur Électronique<br><input type="radio" name="metier" value="sta"> Stagiaire Électronique<br></div><div><input type="submit"></div></form>');
 	        break;
 	    default:
-	    	$(this).after('<form id="fromMetier"><input type="radio" name="metier" value="ing" checked> Ingénieur Informatique<br><input type="radio" name="metier" value="cho"> Chômageur Informatique<br><input type="radio" name="metier" value="sta"> Stagiaire Informatique<br><input type="submit"></form>');
+	    	$(this).after('<form id="fromMetier"><div><input type="radio" name="metier" value="ing" checked> Ingénieur Informatique<br><input type="radio" name="metier" value="cho"> Chômageur Informatique<br><input type="radio" name="metier" value="sta"> Stagiaire Informatique<br></div><div><input type="submit"></div></form>');
 	}
 
 	});	
