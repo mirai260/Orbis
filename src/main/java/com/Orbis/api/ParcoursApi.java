2257184b30c2b3e15fe1922c20934f067a3cdd8c
Al7Mino <al7.deniau@gmail.com>
2018-06-09 18:07:36 +0200

    Api metierParcours

--- src/main/java/com/Orbis/api/ParcoursApi.java
+++ src/main/java/com/Orbis/api/ParcoursApi.java
@@ -19,12 +19,27 @@ public interface ParcoursApi {
             @ApiResponse(code = 200 /*code reponse*/, message = "Success", response = Prerequis.class /*type de retour*/),
             @ApiResponse(code = 404 /*code reponse*/, message = "Prérequis non trouvé", response = Void.class /*type de retour*/)
     })
+    
     @RequestMapping(value = "api/getParcoursWithPrerequis",
             produces = { "application/json" },
             method = RequestMethod.POST /*méthode d'accès à l'api*/)
     ResponseEntity<List<Parcours>/*type de retour*/> getParcoursWithPrerequis(
             @ApiParam(value = "liste des ids des prérequis", required = true)
             @RequestBody List<Long>/*type de paramètre*/ listePrerequis    //Paramètre passé en POST (données de formulaire)
+    );
+    
+    @ApiOperation(value = "Récupérer les parcours liés à ces métiers", notes = "", response = Prerequis.class, tags={ "parcours", })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200 /*code reponse*/, message = "Success", response = Prerequis.class /*type de retour*/),
+            @ApiResponse(code = 404 /*code reponse*/, message = "Métier non trouvé", response = Void.class /*type de retour*/)
+    })
+    
+    @RequestMapping(value = "api/getParcoursByMetiers",
+            produces = { "application/json" },
+            method = RequestMethod.POST /*méthode d'accès à l'api*/)
+    ResponseEntity<List<Parcours>/*type de retour*/> getParcoursByMetiers(
+            @ApiParam(value = "liste des ids des métiers", required = true)
+            @RequestBody List<Long>/*type de paramètre*/ listeMetiers    //Paramètre passé en POST (données de formulaire)
     );
 
 }
