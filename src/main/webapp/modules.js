3af9d0c8d2603b79d80b95bf6ff17649de616e69
legyllith <jeremy.n.49@live.fr>
2018-06-12 17:19:45 +0200

    petit bouton

--- src/main/webapp/modules.js
+++ src/main/webapp/modules.js
@@ -9,7 +9,10 @@ $(document).ready(function(){
 			"Content-Type":"application/x-www-form-urlencoded; charset=utf-8"
 		},
 		success: function(data) {
-			console.log(data);
+			parcours = data;
+			for(i in parcours) {
+				$("#Parcours").append('<div class="positionParcour" onClick="alert(\'coucou\');">'+parcours[i].nom+'</div>');
+			}
 		},
 		error: function(data) {
 			console.log(data);
@@ -17,10 +20,39 @@ $(document).ready(function(){
 		contentType: "application/json; charset=utf-8",
 		async: false
 	})
+});
+
+function ajouteHTMLajoueconcept(){
+	$("#ajouteConcept").empty();
+	$("#ajouePrerequis").empty();
+	$("#ajouteConcept").append('<div id="selectAjouteConcept"></div>');
+	ajouteSelect1();
+	
+}
+
+function ajouteSelect1(){
+	var domainesCD;
 	$.post('api/getAllConcepts', function(data) {
-		parcours = data;
-		for(i in parcours) {
-			$("#Parcours").append('');
+		domainesCD = data;
+		for(i in domainesCD) {
+			$("#selectAjouteConcept").append('<input type="checkbox" value="'+domainesCD[i].id_concept+'"/><label>'+domainesCD[i].nom+'</label><br>');
 		}
 	});
+}
+
+function ajouteHTMLajouePrerequis(){
+	$("#ajouePrerequis").empty();
+	$("#ajouteConcept").empty();
+	$("#ajouePrerequis").append('<div id="selectAjoutePrerequis"></div>');
+	ajouteSelect2();
+	
+}
+function ajouteSelect2(){
+	var domainesCD;
+	$.get('api/getAllPrerequis', function(data) {
+		domainesCD = data;
+		for(i in domainesCD) {
+			$("#selectAjoutePrerequis").append('<input type="checkbox" value="'+domainesCD[i].id_concept+'"/><label>'+domainesCD[i].nom+'</label><br>');
+		}
 	});
+}
